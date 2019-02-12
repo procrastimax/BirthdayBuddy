20e9d26433eba499d44f2741e4dbbee97b262e48
handsome-max <heykeroth.dev@gmail.com>
2019-02-13 00:40:01 +0100

    [ADD] base eventday class

---
+++ app/src/main/java/com/procrastimax/birthdaybuddy/models/EventDay.kt
@@ -0,0 +1,14 @@
+package com.procrastimax.birthdaybuddy.models
+
+/**
+ * A *model* class to provide basic event data.
+ *
+ * This is a base class to be derived from.
+ * Its purpose is to maintain a basic event logic/data, f.e. store a date and a description
+ * and check wether this date is valid.
+ *
+ * @author Procrastimax
+ */
+class EventDay {
+
+}
