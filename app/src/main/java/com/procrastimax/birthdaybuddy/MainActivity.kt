5f1023d7adfc9836d39af13452a459d79b2c2fa1
Procrastimax <heykeroth.dev@gmail.com>
2019-05-02 00:23:26 +0200

    [REMOVE] uneccessary toolbar xmls

--- app/src/main/java/com/procrastimax/birthdaybuddy/MainActivity.kt
+++ app/src/main/java/com/procrastimax/birthdaybuddy/MainActivity.kt
@@ -2,7 +2,6 @@ package com.procrastimax.birthdaybuddy
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.design.widget.CoordinatorLayout
@@ -10,7 +9,6 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.util.TypedValue
-import android.view.View
 import android.widget.ProgressBar
 import com.procrastimax.birthdaybuddy.fragments.*
 import com.procrastimax.birthdaybuddy.handler.BitmapHandler
